72b7d8ea8b90369061af4578ede402c65f12eabe
1027-R <yrk1027@naver.com>
2023-10-10 15:18:14 +0900

    202310101517
    202310101517

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230926.1956';
+const cacheName = 'chirpy-20231010.1517';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
